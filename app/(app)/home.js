67dd99ad1218a6477b3ef78b0e3b173ebed8f4c4
danielpsrb <danielpsrb01@gmail.com>
2024-04-13 22:38:30 +0700

    add logout to home

--- app/(app)/home.js
+++ app/(app)/home.js
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button } from 'react-native';
+import React from 'react';
+import  { useAuth } from '../../context/authContext';
 
 export default function Home() {
+
+    const { logout } = useAuth();
+    const handleLogout = async () => {
+        await logout();
+    }
+
     return (
         <View>
-            <Text>Home yah</Text>
+            <Text>Home</Text>
+            <Button title="Logout" onPress={handleLogout}>
+                <Text>Sign Out</Text>
+            </Button>
         </View>
     )
 }
