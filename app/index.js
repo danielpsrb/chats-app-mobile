6925893ec0eedef2eac1a76567d248df73d77603
danielpsrb <danielpsrb01@gmail.com>
2024-04-14 11:41:52 +0700

    updated index.js

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ import { View, Text, ActivityIndicator } from 'react-native'
 import React from 'react'
 import { Slot } from 'expo-router'
 
-export default function Home() {
+export default function StartPage() {
     return (
         <View className="flex-1 justify-center items-center">
             <ActivityIndicator size="large" color="blue" />
